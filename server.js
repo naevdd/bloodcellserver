d4729d18ad5918514c7e427121f7eb3669d878e5
Mathphile2807 <149683187+Mathphile2807@users.noreply.github.com>
2025-01-10 12:29:01 +0530

    changed username and password

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const adminRoutes = require('./routes/adminRoutes')
-const donorRoutes = require('./routes/donorRoutes');
-const patientRoutes = require('./routes/patientRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const adminRoutes = require("./routes/adminRoutes");
+const donorRoutes = require("./routes/donorRoutes");
+const patientRoutes = require("./routes/patientRoutes");
 
 dotenv.config();
 
@@ -14,27 +14,30 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://bloodcell-client.onrender.com',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(session({
-    secret: 'your-secret-key',  
+app.use(
+  session({
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } 
-  }));
-app.use('/api/admin', adminRoutes);
-app.use('/api/donor', donorRoutes);
-app.use('/api/patient', patientRoutes);
+    cookie: { secure: false },
+  })
+);
+app.use("/api/admin", adminRoutes);
+app.use("/api/donor", donorRoutes);
+app.use("/api/patient", patientRoutes);
 
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
   );
 }
 
